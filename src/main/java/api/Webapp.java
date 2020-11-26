11a3900dbb51e4bee41767ed500c14896d17e348
jensgelbek <jensgelbek@gmail.com>
2020-11-26 20:20:01 +0100

    kan nu vise emils navbar

--- src/main/java/api/Webapp.java
+++ src/main/java/api/Webapp.java
@@ -1,19 +1,13 @@
 package api;
 
-import domain.items.InvalidItem;
-import domain.items.Item;
-import domain.items.ItemNotFound;
-import domain.items.ItemRepository;
-import infrastructure.DBItems;
-
-public class Webapp implements ItemRepository {
+public class Webapp {
     private final static int VERSION = 1;
-    private final static String TITLE = "WebApp Template";
+    private final static String TITLE = "Fog";
     
-    private final DBItems dbItems;
+  ;
     
     public Webapp() {
-        dbItems = new DBItems();
+
     }
     
     public static int getVersion() {
@@ -23,28 +17,9 @@ public class Webapp implements ItemRepository {
     public static String getTitle() {
         return TITLE;
     }
+
+
+
     
-    public Item findItem(int id) throws ItemNotFound {
-        try{
-            return dbItems.findItem(id);
-        } catch (ItemNotFound e){
-            throw new ItemNotFound(e.getMessage());
-        }
-    }
-    
-    public Iterable<Item> findAllItems() throws ItemNotFound {
-        try {
-            return dbItems.findAllItems();
-        } catch (ItemNotFound e){
-            throw new ItemNotFound(e.getMessage());
-        }
-    }
-    
-    public Item createItem(String itemName) throws InvalidItem {
-        try {
-            return dbItems.createItem(itemName);
-        } catch (InvalidItem e){
-            throw new InvalidItem();
-        }
-    }
+
 }
